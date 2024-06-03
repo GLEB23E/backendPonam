256a4d202f2e6b508e0000789cf9f977cde9e383
GLEB23E <132079404+GLEB23E@users.noreply.github.com>
2024-06-03 14:26:28 +0300

    v1

---
+++ src/api/optionsprod/routes/optionsprod.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * optionsprod router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::optionsprod.optionsprod');
---
+++ src/api/optionsprod/services/optionsprod.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * optionsprod service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::optionsprod.optionsprod');
